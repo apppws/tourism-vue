630df239a766ed6c6b08da952720e7d891c1f492
pws <1771134069@qq.com>
2019-01-09 09:23:51 +0800

    首页的自动加载(懒加载)

--- src/views/Index/index.vue
+++ src/views/Index/index.vue
@@ -17,7 +17,14 @@
       </header>
       <!-- 搜索 S -->
       <section class="search-wrapper" data-v-0ae9634c>
-        <input id="search" type="search" v-model="search" @click="dosearch" placeholder="搜索目的地／游记攻略／问答" ref="input1">
+        <input
+          id="search"
+          type="search"
+          v-model="search"
+          @click="dosearch"
+          placeholder="搜索目的地／游记攻略／问答"
+          ref="input1"
+        >
         <!---->
       </section>
       <!-- 搜索 E -->
@@ -96,7 +103,14 @@
         </div>
         <div class="raider-box">
           <router-link to="tourlist">
-            <div class="raider-sub" v-for="(v,k) in tourblogs" :key="k">
+            <div
+              class="raider-sub"
+              v-for="(v,k) in tourblogs"
+              :key="k"
+              v-infinite-scroll="loadMore"
+              infinite-scroll-disabled="disableLoad"
+              infinite-scroll-distance="50"
+            >
               <div class="raider-left">
                 <div class="raider-title">{{ v.title }}</div>
                 <div class="raider-text">{{v.content}}</div>
@@ -144,10 +158,9 @@
                 <img src="../../assets/img/guide_pic.png" alt>
               </div>
             </div>
-          </a> -->
-
-          <wv-loadmore></wv-loadmore>
-          <wv-loadmore type="lineDot" text="loading"></wv-loadmore>
+          </a>-->
+          <wv-loadmore v-if="islastPage" id="stopLoad" type="lineDot" text="loading"></wv-loadmore>
+          <wv-loadmore v-else></wv-loadmore>
         </div>
       </section>
     </div>
@@ -158,7 +171,7 @@ export default {
   data() {
     return {
       // 搜索
-      search:'',
+      search: "",
       // 推荐数据
       tourlist: {
         logo: "",
@@ -179,15 +192,20 @@ export default {
         }
       },
       // 游记日志
-      tourblogs: {
-        title: "",
-        content:'',
-        zan: '',
-        shoucang: '',
-        img: ""
+      tourblogs: [],
+      listsearch: {
+        page: 4,
+        per_page: 3,
+        sortby: "id",
+        order: "desc"
       },
+      // 是否禁止滚动
+      disableLoad: false,
+      //是否到最后一页
+      islastPage: false
     };
   },
+  mounted() {},
   created() {
     // 推荐
     this.axios
@@ -199,19 +217,48 @@ export default {
         this.tourlist = res.data.data;
       });
     // 游记日志
-    this.axios.get("https://easy-mock.com/mock/5c3305b6c0a7f916f9116d93/tour/tourblogs",this.tourblogs)
-    .then((res)=>{
-      this.tourblogs = res.data.data;
-      // console.log(this.tourblogs);
-    })
+    this.axios
+      .get(
+        "https://easy-mock.com/mock/5c3305b6c0a7f916f9116d93/tour/tourblogs",
+        this.tourblogs
+      )
+      .then(res => {
+        this.tourblogs = res.data.data;
+        // console.log(this.tourblogs);
+      });
   },
   methods: {
-    dosearch(){
-        // 去掉左右空格
-        this.search = this.search.trim();
-        // 调转到搜索页面
-          this.$router.push('/seach');
+    dosearch() {
+      // 去掉左右空格
+      this.search = this.search.trim();
+      // 调转到搜索页面
+      this.$router.push("/seach");
+    },
+    // 当滚动条到 50 触发的函数
+    loadMore() {
+      // 1 先禁止滚动条的滚动
+      this.disableLoad = true;
+      this.listsearch.page++
+      // 3 
+      this.axios.get(
+          "https://www.easy-mock.com/mock/5c3305b6c0a7f916f9116d93/tour/tourblogs",
+          { params: this.listsearch }
+        )
+        .then(res => {
+          console.log(res.data.data)
+          // 合并到原来数组
+          this.tourblogs = this.tourblogs.concat(res.data.data);
+          // 在判断这个数据有几条记录  
+          // console.log(Math.floor(Object.keys(this.tourblogs).length / this.listsearch.page));
+          if ( Math.floor(Object.keys(this.tourblogs).length / this.listsearch.per_page) > 5) {
+          //   // 设置允许访问
+            this.disableLoad = false;
+          } else {
+            // 已经到最后一页了
+            this.islastPage = true;
+          }
+        });
     }
-  },
+  }
 };
 </script>
